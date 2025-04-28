4ae97fffed8521afd4bff4c0e2746c93654a6041
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-04-28 14:04:41 +0300

    added the to event button in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,64 @@
 import React from "react";
 import AuthFormToggle from "../logins/FormToggle";
 import { useAuth } from "../logins/AuthContext";
-import "./Home.css"; // You'll add your styling here
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   const { currentUser, logout, isAuthenticated } = useAuth();
-
+  const navigate = useNavigate();
+  const handleGoToEvents = () => {
+    navigate("/events");
+  };
   return (
     <div className="login-page">
       {isAuthenticated ? (
-        <div className="login-box" style={{ backgroundColor: "#fff", padding: "40px", borderRadius: "20px" }}>
+        <div
+          className="login-box"
+          style={{
+            backgroundColor: "#fff",
+            padding: "40px",
+            borderRadius: "20px",
+          }}
+        >
           <h1>Welcome to EventEase</h1>
           <p>Hello, {currentUser.name}!</p>
-          <button className="btn-primary" onClick={logout}>Logout</button>
+          <div>
+            <button className="btn-primary" onClick={handleGoToEvents}>
+              Go to Events
+            </button>
+            <button className="btn-primary" onClick={logout}>
+              Logout
+            </button>
+          </div>
         </div>
       ) : (
         <div className="login-box">
           {/* Left side: Form & Text */}
           <div className="login-left">
-            <h1>WELCOME<br />BACK</h1>
-            <p>Need an account? <strong>Sign Up</strong></p>
+            <h1>
+              WELCOME
+              <br />
+              BACK
+            </h1>
+            <p>
+              Need an account? <strong>Sign Up</strong>
+            </p>
 
             <AuthFormToggle />
 
             <div className="social-login">
-              <button><img src="/google-icon.svg" alt="Google" /></button>
-              <button><img src="/microsoft-icon.svg" alt="Microsoft" /></button>
-              <button><img src="/apple-icon.svg" alt="Apple" /></button>
+              <button>
+                <img src="/google-icon.svg" alt="Google" />
+              </button>
+              <button>
+                <img src="/microsoft-icon.svg" alt="Microsoft" />
+              </button>
+              <button>
+                <img src="/apple-icon.svg" alt="Apple" />
+              </button>
             </div>
           </div>
-
-          {/* Right side: Image */}
           <div className="login-right">
             <img src="src/pages/EVimage/download (1).jpg" alt="Login visual" />
           </div>
@@ -41,4 +69,3 @@ const Home = () => {
 };
 
 export default Home;
- 
